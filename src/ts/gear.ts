9b05d0f2c5c23fd8b3fae5d75bee1e618bb70c92
Jérémie Piellard <jeremie.piellard@gmail.com>
2023-02-24 00:10:52 +0100

    Better shapes

--- src/ts/gear.ts
+++ src/ts/gear.ts
@@ -1,8 +1,14 @@
 import { Point } from "./point";
 
-type Part = {
+type Ray = {
     radius: number;
     angle: number;
+};
+
+type TryResult = {
+    distance: number;
+    periodicity: number;
+    error: number;
 };
 
 class Gear {
@@ -23,7 +29,7 @@ class Gear {
 
             for (const gear of gears) {
                 context.beginPath();
-                gear.parts.forEach((part: Part, index: number) => {
+                gear.rays.forEach((part: Ray, index: number) => {
                     const point = {
                         x: gear.center.x + part.radius * Math.cos(part.angle + gear.rotation),
                         y: gear.center.y + part.radius * Math.sin(part.angle + gear.rotation),
@@ -56,7 +62,7 @@ class Gear {
                 normalize(center);
                 context.moveTo(center.x, center.y);
 
-                const part = gear.parts[0];
+                const part = gear.rays[0];
                 if (!part) {
                     throw new Error("Gear has no parts.");
                 }
@@ -86,21 +92,22 @@ class Gear {
         }
     }
 
+    public periodicity: number = 1;
     public rotation: number = 0;
     private readonly center: Point;
 
-    private readonly parts: Part[] = [];
+    private readonly rays: Ray[] = [];
 
     public constructor() {
         this.center = { x: 0, y: 0 };
 
         const step = 2 * Math.PI / 360;
 
-        const a = 0.5;
-        const b = 0.3;
+        const a = 0.3;
+        const b = 0.15;
 
         for (let i = 0; i < 2 * Math.PI; i += step) {
-            this.parts.push({
+            this.rays.push({
                 radius: a * b / Math.sqrt(Math.pow(b * Math.cos(i), 2) + Math.pow(a * Math.sin(i), 2)),
                 angle: i,
             });
@@ -108,21 +115,102 @@ class Gear {
     }
 
     public change(center: Point, other: Gear): void {
-        this.center.x = center.x;
-        this.center.y = center.y;
+        const dX = center.x - other.center.x;
+        const dY = center.y - other.center.y;
+        const idealDistance = Math.sqrt(dX * dX + dY * dY);
 
-        this.parts.length = 0;
+        const initialTry = this.tryChange(idealDistance, other);
 
-        const dX = this.center.x - other.center.x;
-        const dY = this.center.y - other.center.y;
-        const baseDistance = Math.sqrt(dX * dX + dY * dY);
+        let tooLowTry = initialTry;
+        let tooHighTry = null as TryResult | null;
 
-        for (const otherPart of other.parts) {
-            this.parts.push({
-                radius: baseDistance - otherPart.radius,
-                angle: Math.PI - otherPart.angle,
-            });
+        const maxTries = 200;
+        let triesCount = 1;
+        while (tooLowTry.error > 0.0001 && triesCount < maxTries) {
+            const currentDistance = tooHighTry ? 0.5 * (tooLowTry.distance + tooHighTry.distance) : tooLowTry.distance + 1;
+            if (currentDistance === tooLowTry.distance || currentDistance === tooHighTry?.distance) {
+                // console.log("Convergence");
+                break;
+            }
+
+            const currentTry = this.tryChange(currentDistance, other);
+            // let gap = tooHighTry ? tooHighTry?.distance - tooLowTry.distance : null;
+            // console.log(`distance: ${currentTry.distance}\t\t\terror ${currentTry.error}\t\tgap ${gap}\t\ttoo low ${JSON.stringify(tooLowTry)}\t\ttoo high ${JSON.stringify(tooHighTry)}`);
+            if (currentTry.periodicity > tooLowTry.periodicity || currentTry.error > tooLowTry.error) {
+                tooHighTry = currentTry;
+            } else {
+                tooLowTry = currentTry;
+            }
+            triesCount++;
         }
+
+        const finalTry = tooLowTry;
+        console.log(`Final error ${finalTry.error} obtained in ${triesCount} tries. Final periodicity ${finalTry.periodicity}, initial was ${initialTry.periodicity}.`);
+
+        this.center.x = finalTry.distance;
+        this.center.y = 0;
+        this.periodicity = finalTry.periodicity;
+        // this.rotation = Math.atan2(dY, dX);
+        // this.center.x = finalTry.distance * Math.cos(this.rotation);
+        // this.center.y = finalTry.distance * Math.sin(this.rotation);
+    }
+
+    /* Returns matching score */
+    private tryChange(distance: number, other: Gear): TryResult {
+        this.rays.length = 0;
+
+        const otherFirstRay = other.rays[0];
+        if (!otherFirstRay) {
+            throw new Error("No ray :(");
+        }
+
+        const initialAngle = Math.PI - otherFirstRay.angle;
+        let angle = initialAngle;
+        this.rays.push({
+            radius: distance - otherFirstRay.radius,
+            angle,
+        });
+
+        let periodicity = 1;
+        let i = 0;
+        while (1) {
+            const otherRay1 = other.rays[i % other.rays.length]!;
+            const otherRay2 = other.rays[(i + 1) % other.rays.length]!;
+
+            const otherR1 = otherRay1.radius;
+            const otherR2 = otherRay2.radius;
+            const dOtherAngle = otherRay2.angle - otherRay1.angle;
+            const dSegmentLengthSquared = (otherR1 * otherR1) + (otherR2 * otherR2) - 2 * otherR1 * otherR2 * Math.cos(dOtherAngle);
+
+            const r1 = distance - otherR1;
+            const r2 = distance - otherR2;
+            const dAngle = Math.acos((r1 * r1 + r2 * r2 - dSegmentLengthSquared) / (2 * r1 * r2));
+            if (isNaN(dAngle)) {
+                throw new Error(":(");
+            }
+
+            angle -= dAngle;
+            if (Math.abs(angle - initialAngle) >= 2 * Math.PI) {
+                break;
+            }
+            this.rays.push({
+                radius: r2,
+                angle,
+            });
+
+            i++;
+
+            if (i >= other.rays.length) {
+                periodicity++;
+                i %= other.rays.length;
+            }
+        }
+
+        return {
+            distance,
+            periodicity,
+            error: other.rays.length - i,
+        };
     }
 }
 
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,11 +8,11 @@ function getContext(): CanvasRenderingContext2D {
         throw new Error("Failed to get context.");
     }
 
-    const updateCanvasSize = () => {
+    const updateCanvasSize = (): void => {
         const size = Page.Canvas.getSize();
         context.canvas.width = size[0];
         context.canvas.height = size[1];
-    }
+    };
 
     Page.Canvas.Observers.canvasResize.push(updateCanvasSize);
     updateCanvasSize();
@@ -27,14 +27,18 @@ function main(): void {
 
     other.change({ x: 0.7, y: 0 }, gear);
 
+    Page.Canvas.Observers.mouseMove.push((): void => {
+        if (Page.Canvas.isMouseDown()) {
+            const mousePosition = Page.Canvas.getMousePosition();
+            other.change({ x: 2 * mousePosition[0] - 1, y: 2 * mousePosition[1] - 1 }, gear);
+        }
+    });
+
     function mainLoop(): void {
         context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
-        const mousePosition = Page.Canvas.getMousePosition();
-        other.change({ x: 2 * mousePosition[0] - 1, y: 2 * mousePosition[1] - 1 }, gear);
-
-        // gear.rotation = Math.PI / 180 * performance.now() / 30;
-        // other.rotation = Math.PI - gear.rotation;
+        gear.rotation = Math.PI / 180 * performance.now() / 20;
+        other.rotation = (Math.PI - gear.rotation) / other.periodicity;
 
         Gear.draw(context, gear, other);
         requestAnimationFrame(mainLoop);
